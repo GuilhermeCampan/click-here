a316eff73924257a9862ac9c90e23ae8247fc7ae
Guilherme Campano <campano.guilherme@gmail.com>
2018-04-05 14:15:03 +0100

    Add UT to Stages

--- src/stages/BasicStage/BasicStage.js
+++ src/stages/BasicStage/BasicStage.js
@@ -2,16 +2,7 @@ import React from "react";
 import ClickHere from "./ClickHere";
 
 export default class BasicStage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  stageComplete = () => {
-    console.log("Stage 1 completed!");
-  };
-
   render() {
-    const stageName = "Stage 1";
     return (
       <div>
         <ClickHere handleClick={this.props.completeStage()} />
