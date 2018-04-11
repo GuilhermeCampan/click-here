de67564c272e58b250f3cfb329b641ebbc6afe07
Bruna Campano <brunacampano@Brunas-MacBook-Pro.local>
2018-04-11 23:33:53 +0100

    Suffle the buttons of DontClickStage

--- src/stages/DontClickStage/DontClickStage.js
+++ src/stages/DontClickStage/DontClickStage.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 import { ClickHere } from '../../common';
 import './DontClickStage.css';
 
-const DontClickStage = ({ nextStage, previousStage }) => (
+const DontClickStage = ({ nextStage, previousStage }) => {
+  const buttons = [
+    <ClickHere key="wrong" handleClick={previousStage()} buttonText='Dont Click Here!' />,
+    <ClickHere key="correct" handleClick={nextStage()} />
+  ];
+
+  const shuffledButtons = buttons.sort(() => .5 - Math.random());
+
+  return (
   <div className="dont-click-stage">
-    <ClickHere handleClick={previousStage()} buttonText='Dont Click Here!' />
-    <ClickHere handleClick={nextStage()} />
+    {shuffledButtons}
   </div>
-);
+  );
+};
 
 DontClickStage.propTypes = {
   nextStage: PropTypes.func,
